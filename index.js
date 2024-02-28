5a3353d3c4bbf70064303ec190aaa291604ce868
jisjis-ai <125311127+jisjis-ai@users.noreply.github.com>
2024-02-28 23:50:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const affiliateId = urlParams.get('id');
 
     // Inicializar Firebase
-    const firebaseConfig = {
-        apiKey: "YOUR_API_KEY",
-        authDomain: "YOUR_AUTH_DOMAIN",
-        projectId: "YOUR_PROJECT_ID",
-        storageBucket: "YOUR_STORAGE_BUCKET",
-        messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-        appId: "YOUR_APP_ID"
-    };
-
+const firebaseConfig = {
+    apiKey: "AIzaSyCfeWv0woxIsO0yeaCZcyqNNpiHY81NIO4",
+    authDomain: "bezcrypto-banco-de-dados.firebaseapp.com",
+    databaseURL: "https://bezcrypto-banco-de-dados-default-rtdb.firebaseio.com",
+    projectId: "bezcrypto-banco-de-dados",
+    storageBucket: "bezcrypto-banco-de-dados.appspot.com",
+    messagingSenderId: "994447258427",
+    appId: "1:994447258427:web:5479a4a067124bb0407d1e",
+    measurementId: "G-DT0BRKVT6P"
+  };
     firebase.initializeApp(firebaseConfig);
     const db = firebase.database();
 
